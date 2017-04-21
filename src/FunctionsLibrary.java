5d4cdae7416e6c81ced440d5b4dd5c23474fe99f
francomoglia496 <francomoglia496@gmail.com>
2017-04-21 00:24:35 -0300

    update

--- src/FunctionsLibrary.java
+++ src/FunctionsLibrary.java
@@ -1,7 +1,10 @@
 import org.apache.commons.net.ftp.FTPClient;
+import sun.util.resources.cldr.de.CalendarData_de_LI;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,7 +13,7 @@ import java.util.HashMap;
  */
 public class FunctionsLibrary {
 
-    public static void connecToServer(){
+    public static void connecToServer() {
 
         FTPClient client = new FTPClient();
 
@@ -38,8 +41,59 @@ public class FunctionsLibrary {
             } catch (IOException e) {
             }
 
-        } catch (IOException ioe){}
+        } catch (IOException ioe) {
+        }
 
     }
 
+    public static void descargarArchivo(){
+
+        // get an ftpClient object
+        FTPClient ftpClient = new FTPClient();
+        FileOutputStream fos = null;
+
+        try {
+            // pass directory path on server to connect
+            ftpClient.connect("ftp.justiciachaco.gov.ar");
+
+            // pass username and password, returned true if authentication is
+            // successful
+            boolean login = ftpClient.login("anonymous", "anonymous");
+
+            ftpClient.enterLocalPassiveMode();
+            if (login) {
+                System.out.println("Connection established...");
+
+                ftpClient.changeWorkingDirectory("/listas/C_A_Civ_y_Com_Sala_I");
+
+                fos = new FileOutputStream("/home/franco/archivo.txt");
+                boolean download = ftpClient.retrieveFile("Cam_Civ_Sala_I_2017-04-20.Txt",
+                        fos);
+                if (download) {
+                    System.out.println("File downloaded successfully !");
+                } else {
+                    System.out.println("Error in downloading file !");
+                }
+
+                // logout the user, returned true if logout successfully
+                boolean logout = ftpClient.logout();
+                if (logout) {
+                    System.out.println("Connection close...");
+                }
+            } else {
+                System.out.println("Connection fail...");
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ftpClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,21 @@ public class Main {
 
     public static void main(String[] args){
 
-        //FTPClient client = new FTPClient();
+        FTPClient client = new FTPClient();
+
 
         //FunctionsLibrary.connecToServer();
 
-        Localidad resistencia = Localidad.resistencia();
+        /*Localidad resistencia = Localidad.resistencia();
 
         for (String a : resistencia.getCaratulas()) {
 
             System.out.println(a);
 
-        }
+        }*/
+
+        FunctionsLibrary.descargarArchivo();
+
 
     }
 
